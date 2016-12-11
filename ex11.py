7030d0c99d6edabf594c5a91cde3514f87d1c586
Rob Donoghue <rdonoghue@gmail.com>
2016-12-11 08:53:55 -0500

    Examples up to 15 - https://learnpythonthehardway.org/book/ex15.html

---
+++ ex11.py
@@ -0,0 +1,13 @@
+## print "How old are you?",
+age=raw_input("How old are you?")
+print "How tall are you?",
+height=raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+
+print "So you're %r old, %r tall and weigh %r." % (
+age,
+height,
+weight
+)
---
+++ ex12.py
@@ -0,0 +1,13 @@
+## print "How old are you?",
+age=raw_input("How old are you?")
+height=raw_input("How tall are you?")
+
+weight = raw_input("How much do you weigh?")
+
+
+
+print "So you're %r old, %r tall and weigh %r." % (
+age,
+height,
+weight
+)
